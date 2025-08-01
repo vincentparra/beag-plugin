9ab4f996f840825cce4e1b5bf2d6a2c312af6c20
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-08-01 18:04:59 +0800

    added plugin file config (#1)

--- src/main/java/com/rocs/blocking/embedded/ai/generated/code/plugin/reports/FeatureReportInterface.java
+++ src/main/java/com/rocs/blocking/embedded/ai/generated/code/plugin/reports/FeatureReportInterface.java
@@ -3,6 +3,9 @@ package com.rocs.blocking.embedded.ai.generated.code.plugin.reports;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
+import java.nio.file.Path;
+import java.util.List;
+
 public interface FeatureReportInterface {
-    void getReports() throws MojoExecutionException, MojoFailureException;
+    void getReports(List<Path> javaFiles) throws MojoExecutionException, MojoFailureException;
 }
