209b7529ac3a94eb4bf61a01296cd0faae62e851
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-09-18 14:21:49 +0800

    added new feature

---
+++ src/main/java/com/rocs/blocking/embedded/ai/generated/code/plugin/exception/FileNotFoundException.java
@@ -0,0 +1,9 @@
+package com.rocs.blocking.embedded.ai.generated.code.plugin.exception;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+public class FileNotFoundException extends MojoExecutionException {
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+}
